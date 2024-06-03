db2baaa848a1d7f166f33514e4c7fc84121ad3b1
Lucrecia Martin <lucre.martin01@gmail.com>
2024-06-03 19:31:15 -0300

    View

---
+++ src/main/java/org/example/view/PanelLogin.java
@@ -0,0 +1,11 @@
+package org.example.view;
+
+import javax.swing.*;
+
+public class PanelLogin {
+    private JButton login;
+    private JPasswordField passwordField1;
+    private JFormattedTextField formattedTextField1;
+
+
+}
